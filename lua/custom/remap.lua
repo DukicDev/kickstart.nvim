049441d8c4d887824cff0d914676048010f6e51b
ddukic <dino.dukic@wu.ac.at>
2024-09-25 13:17:26 +0200

    Add remap for editing init.lua

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>i', '<cmd>e $MYVIMRC<CR>', { desc = 'Open init.lua (nvim config)' })
