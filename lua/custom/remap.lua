f8cb468547a425f5a677312d3737953729f07f92
DukicDev <dukic.dev@gmail.com>
2024-09-28 13:35:14 +0200

    Remap Search Moving and Half-Page Moving to Center Cursor afterward

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -1,2 +1,9 @@
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('n', '<leader>i', '<cmd>e $MYVIMRC<CR>', { desc = 'Open init.lua (nvim config)' })
+
+-- Center Cursor after Search or Page Moving
+vim.keymap.set('n', 'n', 'nzz', { noremap = true })
+vim.keymap.set('n', 'N', 'Nzz', { noremap = true })
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
