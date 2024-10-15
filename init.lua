b4e6c48ea768f52ddacdacbb35b451417240ad60
DukicDev <dukic.dev@gmail.com>
2024-10-15 19:41:24 +0200

    Revert "Revert "Add nvim-notify""
    This reverts commit 5784126bbde0b0ca367bce1e6f7d2c86f5157891.

--- init.lua
+++ init.lua
@@ -157,9 +157,11 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- 24-bit color for nvim-notify
+vim.opt.termguicolors = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
-
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
@@ -246,6 +248,19 @@ require('lazy').setup({
     -- this is equivalent to setup({}) function
   },
   'nvim-treesitter/nvim-treesitter-context',
+  'rcarriga/nvim-notify',
+
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+    config = true,
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -426,7 +441,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>s.', function()
+        builtin.find_files { no_ignore = true }
+      end, { desc = 'Search with Hidden Files' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
@@ -1049,5 +1066,11 @@ rt.setup {
     end,
   },
 }
+
+vim.notify = require 'notify'
+
+-- vim.o.background = 'dark'
+-- vim.cmd [[colorscheme gruvbox]]
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
