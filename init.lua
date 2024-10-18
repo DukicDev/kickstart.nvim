fae468e8828d07f0f1ae094a48d072b5dde09fd9
DukicDev <dukic.dev@gmail.com>
2024-10-18 23:11:07 +0200

    Add Obsidian Plugin

--- init.lua
+++ init.lua
@@ -272,7 +272,33 @@ require('lazy').setup({
     end,
   },
   {
+    'epwalsh/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    -- lazy = true,
+    -- ft = 'markdown',
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   --   -- refer to `:h file-pattern` for more examples
+    --   'BufReadPre /mnt/c/Users/Dino/Documents/Obsidian/Personal/*.md',
+    --   'BufNewFile /mnt/c/Users/Dino/Documents/Obsidian/Personal/*.md',
+    -- },
+    dependencies = {
+      -- Required.
+      'nvim-lua/plenary.nvim',
+
+      -- see below for full list of optional dependencies 👇
+    },
     opts = {
+      workspaces = {
+        {
+          name = 'personal',
+          path = '/mnt/c/Users/Dino/Documents/Obsidian/Personal/',
+        },
+      },
+
+      -- see below for full list of options 👇
     },
   },
 
@@ -1126,6 +1152,10 @@ require('pendulum').setup {
   timeout_len = 300,
 }
 
+-- Obsidian keymaps
+vim.keymap.set('n', '<leader>on', '<cmd>ObsidianNew<cr>', { desc = 'Obsidian New' })
+vim.keymap.set('n', '<leader>oo', '<cmd>ObsidianOpen<cr>', { desc = 'Obsidian Open' })
+vim.keymap.set('n', '<leader>os', '<cmd>ObsidianSearch<cr>', { desc = 'Obsidian Search' })
 
 -- vim.o.background = 'dark'
 -- vim.cmd [[colorscheme gruvbox]]
